4e0e1887a48869d6f7b891bb61633c1500b0dd21
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-01-17 01:19:10 -0600

    Message, rotation box and encrypted message box

--- main.py
+++ main.py
@@ -16,13 +16,28 @@
 #
 import webapp2
 import caesar
+import cgi
+def build_page(textarea_content):
+
+    message_form =  "<form method='post'><label>Message  </label><textarea name='message' cols='600' rows='500'>" + textarea_content + "</textarea></form>"
+    return message_form
 
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
-        encrypted_message = caesar.encrypt("Hello World", 13)
-        self.response.write(encrypted_message)
 
+
+        content =  "<form method='post'><label>Message   </label><textarea name='message'></textarea><br><label>Rorate By </label><input type='number' name='rotation'><input type='submit'></form>"
+        header = "<h2>Web Caesar</h2>"
+        self.response.write( header + content)
+    def post(self):
+        message = self.request.get("message")
+        rotation = self.request.get("rotation")
+        encrypted_message = caesar.encrypt(message, rotation)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
+        header = "<h2>Web Caesar</h2>"
+        self.response.write(header + content)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
