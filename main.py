8cd7949af807d81fb1d8ec82f0a0bd77b15d8d92
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-01-19 19:53:31 -0600

    box sizes adjusted

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import caesar
 import cgi
 def build_page(textarea_content):
 
-    message_form =  "<form method='post'><label>Message  </label><textarea name='message' cols='600' rows='500'>" + textarea_content + "</textarea></form>"
+    message_form =  "<form method='post'><label>Message  </label><textarea name='message' cols='70' rows='20'>" + textarea_content + "</textarea></form>"
     return message_form
 
 class MainHandler(webapp2.RequestHandler):
@@ -27,8 +27,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
 
 
-        content =  "<form method='post'><label>Message   </label><textarea name='message'></textarea><br><label>Rorate By </label><input type='number' name='rotation'><input type='submit'></form>"
-        header = "<h2>Web Caesar</h2>"
+        content =  "<form method='post'><label>Message   </label><textarea name='message'cols='60' rows='15'></textarea><br><label>Rorate By </label><input type='number' name='rotation'><input type='submit'></form>"
+        header = "<h2 text-align='center'>Web Caesar</h2>"
         self.response.write( header + content)
     def post(self):
         message = self.request.get("message")
