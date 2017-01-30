0c3eb86347325cbe3ce8b3accc48578048f80b6a
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-01-29 19:00:43 -0600

    escaped

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import caesar
 import cgi
@@ -33,8 +17,8 @@ class MainHandler(webapp2.RequestHandler):
     def post(self):
         message = self.request.get("message")
         rotation = self.request.get("rotation")
-        escacaped_mess = cgi.escape(message)
-        encrypted_message = caesar.encrypt(escacaped_mess, rotation)
+
+        encrypted_message = caesar.encrypt(message, rotation)
         escaped_message = cgi.escape(encrypted_message)
         content = build_page(escaped_message)
         rotaion_form = "<form><label>Rotate By </label><input type='number' name='rotation'><input type='submit'></form"
