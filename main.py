f412c41cb74be6f8cf0ff399dd485086157f2c00
Abdihakim Abdi <abdihakimabdi8@gmail.com>
2017-01-30 23:54:04 -0600

    Form in consistence fixed

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import caesar
 import cgi
 def build_page(textarea_content):
 
-    message_form =  "<form method='post'><label>Message  </label><textarea name='message' cols='70' rows='20'>" + textarea_content + "</textarea></form>"
+    message_form =  "<form method='post'><label>Message  </label><textarea name='message' cols='70' rows='20'>" + textarea_content + "</textarea><br><label>Rotate By </label><input type='number' name='rotation'><input type='submit'></form>"
     return message_form
 
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
-
-
-        content =  "<form method='post'><label>Message   </label><textarea name='message'cols='60' rows='15'></textarea><br><label>Rotate By </label><input type='number' name='rotation'><input type='submit'></form>"
+        content1 = build_page("")
+        content = "<form method='post'><label>Rotate By </label><input type='number' name='rotation'><input type='submit'></form>"
         header = "<h2 text-align='center'>Web Caesar</h2>"
-        self.response.write( header + content)
+        self.response.write( header + content1)
     def post(self):
         message = self.request.get("message")
         rotation = self.request.get("rotation")
@@ -21,9 +20,9 @@ class MainHandler(webapp2.RequestHandler):
         encrypted_message = caesar.encrypt(message, rotation)
         escaped_message = cgi.escape(encrypted_message)
         content = build_page(escaped_message)
-        rotaion_form = "<form><label>Rotate By </label><input type='number' name='rotation'><input type='submit'></form"
+        rotaion_form = "<form method='post'><label>Rotate By </label><input type='number' name='rotation'><input type='submit'></form>"
         header = "<h2>Web Caesar</h2>"
-        self.response.write(header + content + rotaion_form)
+        self.response.write(header + content)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
